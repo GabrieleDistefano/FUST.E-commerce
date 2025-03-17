e9f6ca5c231b9d22c96ec8eeed64a39ebb579dd1
GabrieleDistefano <antipulsaremail@gmail.com>
2025-03-17 09:57:41 +0100

    Creazione progetto

---
+++ FUST.E-commerce/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using FUST.E_commerce.Data;
+
+namespace FUST.E_commerce.Components.Account;
+
+internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+{
+    public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+    {
+        var user = await userManager.GetUserAsync(context.User);
+
+        if (user is null)
+        {
+            redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+        }
+
+        return user;
+    }
+}
